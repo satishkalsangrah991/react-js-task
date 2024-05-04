818d201564ecb1b4d47941de542e464aff286d26
satish kalsangrah <satishkalsangrah991@gmail.com>
2024-05-04 13:55:43 +0530

    04 may

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import "./App.css";
+import Emi from "./components/EmiCalculater/Emi";
 import MenuScrollNavigationBar from "./components/MenuScrollNavigation/MenuScrollNavigationBar";
+import MultiSearchinput from "./components/MultipleSearchInputfeild/MultiSearchinput";
+import MultiImageUpload from "./components/MultipleimagesUpload/MultiImageUpload";
 import MyCurrencyConvert from "./components/MyCurrencyConvert";
+import QuizApp from "./components/QuizApp/QuizApp";
 import TabBar from "./components/TabButton/TabBar";
 import CurrencyConvertor from "./components/currency-convertor";
 import LikeButton from "./components/likebuttontask/LikeButton";
+import Pagination from "./components/pagination/Pagination";
 import ProgressBar from "./components/progressbar/ProgressBar";
-
+import TagsInputField from "./components/tagsinputComponets/TagsInputField";
+const games = ["cricket","footbal","hockey"]
 function App() {
+
+  const checkValue =(c)=>{
+console.log(c)
+  }
   return (
   
     <>
@@ -30,9 +40,11 @@ function App() {
 
   
 {/* tab bar */}
-  <div>
+
+
+  {/* <div>
     <TabBar/>
-  </div>
+  </div> */}
 
      {/* progressbar  */}
        {/* <ProgressBar/> */}
@@ -42,8 +54,54 @@ function App() {
 
 
 
-      
+    {/* <div className="flex justify-center">
+  {
+    games.map((val,index)=>{
+return(
+  <>
+  <div key={index}>
+    <input 
+    type="radio"
+     className="w-5 h-4 mx-2" 
+    id="huey" 
+    name="drone" 
+     value={val} 
+     onClick={()=>checkValue(val)}
+
+     />
+    <label htmlFor={val}>{val}</label>
+  </div>
+  </>
+)
+    })
+  }
+    </div> */}
+
  
+      {/*EMI Calculater  */}
+
+      {/* <Emi/> */}
+
+
+      {/* quiz App */}
+      {/* <QuizApp/> */}
+
+{/* pagination  simple*/}
+      {/* <Pagination/> */}
+
+      {/* tags input componets */}
+      {/* <TagsInputField/> */}
+
+      {/* multiple images */}
+
+      {/* <MultiImageUpload/> */}
+
+
+{/* multipleSearch */}
+{/* <MultiSearchinput/>  */}
+
+
+
   
     </>
   );
